df8842280392c000d6de2d890023e51f03c1d7b4
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-07-12 15:34:03 -0700

    added beginnings of main js to make buttons function as calculator

---
+++ main.js
@@ -0,0 +1,20 @@
+let currentNumber = "";
+let previousNumber = "";
+let operator = "";
+
+const currentDisplayNumber = document.querySelector("currentNumber");
+const previousDisplayNumber = document.querySelector("previousNumber");
+
+const equal = document.querySelector(".equal");
+
+const decimal = document.querySelector(".decimal");
+
+const clear = document.querySelector(".clear");
+
+const numberButtons = document.querySelectorAll(".number");
+
+const operators = document.querySelectorAll(".operator");
+
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {});
+});
