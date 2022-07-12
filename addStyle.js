d1fb1296d2ea1881e2378ecd09253f99835b7ab6
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-07-12 15:13:18 -0700

    corrected calBod class name in js styles

--- addStyle.js
+++ addStyle.js
@@ -2,10 +2,10 @@ const bodyClass = "bg-purple-800 bg-gradient-to-br from-blue-300 bg-opacity-25";
 const bdy = document.querySelector("body");
 bodyClass.split(" ").forEach((cls) => bdy.classList.add(cls));
 
-const calcBodyClass =
+const calBodClass =
   "max-w-lg rounded overflow-hidden shadow-2xl bg-pink-700 bg-opacity-75 bg-gradient-to-tr from-gray-800 flex";
-const calcBody = document.querySelector(".calcBody");
-calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
+const calBod = document.querySelector(".calBod");
+calBodClass.split(" ").forEach((cls) => calBod.classList.add(cls));
 
 const outputClass =
   "flex flex-col w-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
